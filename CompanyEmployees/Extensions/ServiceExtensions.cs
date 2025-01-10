16ea68f2870463e47cfaef4f3f9cccb14169caa7
Anatoli Dobshikov <anatoliy.dobshikov@gmail.com>
2025-01-10 23:29:25 +0300

    Added an extension method to configure the web application CORS policy.
    - CORS policy now allows requests from any source.

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+
+namespace CompanyEmployees.Extensions;
+
+/// <summary>
+/// Contains extension methods to configure web application services.
+/// </summary>
+public static class ServiceExtensions
+{
+    /// <summary>
+    /// Configures the CORS policy to allow requests from any sources.
+    /// </summary>
+    /// <param name="services">Service collection.</param>
+    /// <returns>The same service collection.</returns>
+    public static IServiceCollection ConfigureCORS(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
+
+        return services;
+    }
+}
