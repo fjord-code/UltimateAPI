6f254d85e7d3c8887ec551e8abb786438bc5ddca
Anatoli Dobshikov <anatoliy.dobshikov@gmail.com>
2025-01-22 23:18:09 +0300

    Added the Entities project.

---
+++ Entities/Models/Company.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models;
+
+public class Company
+{
+    [Column("CompanyId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Company name is a required field.")]
+    [MaxLength(60, ErrorMessage = "Maximum length of the company name is 60 characters.")]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "Address is a required field.")]
+    [MaxLength(60, ErrorMessage = "Maximum length of the company address is 60 characters.")]
+    public string? Address { get; set; }
+
+    public string? Country { get; set; }
+
+    public ICollection<Employee>? Employees { get; set; }
+}
---
+++ Entities/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models;
+
+public class Employee
+{
+    [Column("EmployeeId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Employee name is a required field.")]
+    [MaxLength(30, ErrorMessage = "Maximum length of the employee name is 30.")]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "Age is a required field.")]
+    public int Age { get; set; }
+
+    [Required(ErrorMessage = "Position is a required field.")]
+    [MaxLength(20, ErrorMessage = "Maximum length of the employee position is 20.")]
+    public string? Position { get; set; }
+
+    [ForeignKey(nameof(Company))]
+    public Guid? CompanyId { get; set; }
+    public Company? Company { get; set; }
+}
