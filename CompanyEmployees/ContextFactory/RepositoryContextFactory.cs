bb5774f8fecec17ec790794c5fabec0f0adc1f5a
Anatoli Dobshikov <anatoliy.dobshikov@gmail.com>
2025-01-23 22:39:12 +0300

    Added the Repository project.
    - Added the new project.
    - Added migrations.

---
+++ CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace CompanyEmployees.ContextFactory;
+
+public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+{
+    public RepositoryContext CreateDbContext(string[]? args = null)
+    {
+        var builder = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseSqlite(Environment.GetEnvironmentVariable("SqliteConnectionString"),
+                options => options.MigrationsAssembly("CompanyEmployees"));
+
+        return new RepositoryContext(builder.Options);
+    }
+}
