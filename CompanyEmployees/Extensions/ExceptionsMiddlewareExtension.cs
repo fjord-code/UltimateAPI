8340faaeedfbb4ec900f0b8fda70837d3f71692d
Anatoli Dobshikov <anatoliy.dobshikov@gmail.com>
2025-02-05 22:48:45 +0300

    Added a global exceptions handler.

---
+++ CompanyEmployees/Extensions/ExceptionsMiddlewareExtension.cs
@@ -0,0 +1,32 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Diagnostics;
+using Entities.ErrorModel;
+
+namespace CompanyEmployees.Extensions;
+
+public static class ExceptionsMiddlewareExtension
+{
+    public static void ConfigureExceptionHandler(this WebApplication app, ILoggerManager logger)
+    {
+        app.UseExceptionHandler(builder =>
+        {
+            builder.Run(async context =>
+            {
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                if (contextFeature is not null)
+                {
+                    logger.LogError($"Something went wrong: {contextFeature.Error}");
+
+                    await context.Response.WriteAsync(new ErrorDetails
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        Message = "Internal Server Error"
+                    }.ToString());
+                }
+            });
+        });
+    }
+}
