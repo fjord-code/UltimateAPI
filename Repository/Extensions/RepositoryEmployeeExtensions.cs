413f90952a88bd49b56417ed00f98f0bd9944440
Anatoli Dobshikov <anatoliy.dobshikov@gmail.com>
2025-09-09 07:54:46 +0300

    Added sorting.

--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using Entities.Models;
+using System.Linq.Dynamic.Core;
+using System.Reflection;
+using System.Text;
 
 namespace Repository.Extensions
 {
@@ -19,6 +22,47 @@ namespace Repository.Extensions
             var lowerCaseTerm = searchTerm.Trim().ToLower();
 
             return employees.Where(e => !string.IsNullOrEmpty(e.Name) && e.Name.ToLower().Contains(lowerCaseTerm));
+        }
+
+        public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string orderByQueryString)
+        {
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+            {
+                return employees.OrderBy(e => e.Name);
+            }
+
+            var orderParams = orderByQueryString.Trim().Split(',');
+            var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                {
+                    continue;
+                }
+
+                var propertyFromQueryName = param.Split(" ")[0];
+                var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+
+                if (objectProperty is null)
+                {
+                    continue;
+                }
+
+                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+
+                orderQueryBuilder.Append($"{objectProperty.Name} {direction}, ");
+            }
+
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+            if (string.IsNullOrWhiteSpace(orderQuery))
+            {
+                return employees.OrderBy(e => e.Name);
+            }
+
+            return employees.OrderBy(orderQuery);
         }
     }
 }
