97d82c2f9e6420679d04cec61f9d3237d45e18a2
Anatoli Dobshikov <anatoliy.dobshikov@gmail.com>
2025-09-16 22:02:32 +0300

    Added api versioning.

---
+++ CompanyEmployees.Presentation/Controllers/CompaniesV2Controller.cs
@@ -0,0 +1,28 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+
+namespace CompanyEmployees.Presentation.Controllers;
+
+[ApiVersion("2.0", Deprecated = true)]
+[Route("api/companies")]
+[ApiController]
+public class CompaniesV2Controller : ControllerBase
+{
+    private readonly IServiceManager _serviceManager;
+
+    public CompaniesV2Controller(IServiceManager serviceManager)
+    {
+        _serviceManager = serviceManager;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCompanies()
+    {
+        var companies = await _serviceManager.CompanyService.GetAllCompaniesAsync(trackChanges: false);
+
+        var companiesV2 = companies.Select(x => $"{x.Name} V2");
+
+        return Ok(companiesV2);
+    }
+}
