ebe30798a7c33fb2e8510ebc6c85bc400b26f92e
Anatoli Dobshikov <anatoliy.dobshikov@gmail.com>
2025-09-22 21:25:10 +0300

    Added roles.

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -23,7 +23,7 @@ public class CompaniesController : ControllerBase
     }
 
     [HttpGet(Name = "GetCompanies")]
-    [Authorize]
+    [Authorize(Roles = "Manager")]
     public async Task<IActionResult> GetCompanies()
     {
         var companies = await _service.CompanyService.GetAllCompaniesAsync(trackChanges: false);
